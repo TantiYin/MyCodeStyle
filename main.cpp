46db0762a86d440362909b58e80b1e9c19a06f95
tantiyin <iyintao@gmail.com>
2016-08-18 14:46:03 +0800

    全局函数名，首字母要大写

--- main.cpp
+++ main.cpp
@@ -4,19 +4,19 @@
 
 using namespace std;
 
-void execute(CShape* pShape)
+void Execute(CShape* pShape)
 {
 	pShape->Init();
 }
 
-int main(int argc,char** argv)
+int main(int argc, char** argv)
 {
 	CSquare* pSquare;
 	CCircle* pCircle;
 	pSquare = new CSquare(string("squareA"), 2.0);
 	pCircle = new CCircle(string("circleA"), 1.0);
-	execute(pSquare);
-	execute(pCircle);
+	Execute(pSquare);
+	Execute(pCircle);
 
 	system("pause");
 	return 0;
