75d767bde62913aca49cda71d8f66a2044e669cf
tantiyin <iyintao@gmail.com>
2016-09-12 13:15:38 +0800

    删除空行

--- shape.h
+++ shape.h
@@ -9,59 +9,43 @@ class CShape
 {
 public:
     CShape(string& Name): mName(Name), mArea(0)
-    
     {
-        
         ++mNumShapes;
-        
         cout << "There has been " << mNumShapes << " shapes" << endl;
-        
     }
-    
     ~CShape() {--mNumShapes;}
-    
+
     void Init();
-    
     virtual void PrintErea()
-    
     {
-        
         cout << mName << ": " << mArea << endl;
-        
     }
-    
     virtual double GetArea() = 0;
-    
+
 public:
     static int mNumShapes;
-    
+
 private:
     string mName;
-    
     double mArea;
-    
 };
 
 class CSquare: public CShape
 {
 public:
     CSquare(string& Name, double e): CShape(Name), mEdge(e) {}
-    
     virtual double GetArea();
-    
+
 public:
     double mEdge;
-    
 };
 
 class CCircle: public CShape
 {
 public:
     CCircle(string& Name,double R): CShape(Name), mR(R) {}
-    
     virtual double GetArea();
     
 public:
     double mR;
-    
 };
